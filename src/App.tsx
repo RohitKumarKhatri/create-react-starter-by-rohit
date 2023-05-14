d7a739d2354758c952d3e705aa4bc4121f9ef10f
Rohit Kumar Khatri <RohitKumarKhatri@outlook.in>
2023-05-14 11:05:34 +0530

    Add react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const name = 'Rohit'
   return (
     <div className="App">
       <header className="App-header">
@@ -11,12 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React {process.env.name}
         </a>
       </header>
